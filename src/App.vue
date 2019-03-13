84ebaae245d42364cdfc0486f82139db0f523348
baishui <13809898824@139.com>
2019-03-13 20:41:33 +0800

    init again project

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,33 @@
 <template>
-    <div>
+    <div class="app-container">
         <h1>这是app组件</h1>
+
+        <!--顶部 Header 区域-->
+        <mt-header fixed title="fixed top"></mt-header>
+
+        <!--中间 路由Content 区域-->
+
+
+
+        <!--底部 Tabar 区域-->
+        <nav class="mui-bar mui-bar-tab">
+            <a class="mui-tab-item mui-active" href="#tabbar">
+                <span class="mui-icon mui-icon-home"></span>
+                <span class="mui-tab-label">首页</span>
+            </a>
+            <a class="mui-tab-item" href="#tabbar-with-chat">
+                <span class="mui-icon mui-icon-email"><span class="mui-badge">9</span></span>
+                <span class="mui-tab-label">消息</span>
+            </a>
+            <a class="mui-tab-item" href="#tabbar-with-contact">
+                <span class="mui-icon mui-icon-contact"></span>
+                <span class="mui-tab-label">通讯录</span>
+            </a>
+            <a class="mui-tab-item" href="#tabbar-with-map">
+                <span class="mui-icon mui-icon-gear"></span>
+                <span class="mui-tab-label">设置</span>
+            </a>
+        </nav>
 
     </div>
 </template>
@@ -10,5 +37,7 @@
 </script>
 
 <style scoped>
-
+    .app-container {
+        padding-top: 40px;
+    }
 </style>
--- src/main.js
+++ src/main.js
@@ -3,6 +3,8 @@ import $ from 'jquery'
 import './css/index.css'
 import 'bootstrap/dist/css/bootstrap.css'
 
+import './lib/mui/css/mui.min.css'
+
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 Vue.use(VueRouter);
@@ -10,6 +12,11 @@ Vue.use(VueRouter);
 import app from './App.vue'
 
 import router from './router.js'
+
+// 按需导入mint-ui中的组件
+import {Header} from 'mint-ui'
+Vue.component(Header.name, Header);
+
 
 var vm = new Vue({
    el: '#app',
